40868e16097a669f041b96aede880fb3821231e4
Kai Wang <kaiuchihaa@gmail.com>
2022-05-04 22:40:31 -0300

    novo projetos

--- api/index.js
+++ api/index.js
@@ -1,20 +1,12 @@
 
 const { ApolloServer } = require("apollo-server")
 const userSchema = require('./user/schema/user.graphql')
+const userResolvers = require('./user/resolvers/userResolvers.js')
 
-const user = [
-    {
-        nome: "Ana",
-        ativo: true
-    },
-    {
-        nome: "Marcia",
-        ativo: false
-    }
-]
+
 //caso seja nescessário informar mais de um tipo de 
 const typeDefs = [userSchema]
-const resolvers = {}
+const resolvers = [userResolvers]
 
 const server = new ApolloServer({typeDefs, resolvers})
 //caso queira especificara porta pode usar em .listen({port: 4001})
